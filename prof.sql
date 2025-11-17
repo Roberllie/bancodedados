5aa24953380e839eaae494adefa59afb3fa25c59
Roberta Araujo <160649185+Roberllie@users.noreply.github.com>
2025-11-17 17:16:00 -0300

    Create prof.sql

---
+++ prof.sql
@@ -0,0 +1,5 @@
+INSERT INTO professor (nome, email, area)
+VALUES 
+('Marcos Pereira', 'marcos.p@faculdade.edu', 'Matemática'),
+('Ana Ribeiro', 'ana.r@faculdade.edu', 'Computação'),
+('Carlos Silva', 'carlos.s@faculdade.edu', 'Engenharia');
