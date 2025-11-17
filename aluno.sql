e3892f7a5f5305b7a2273e64b5a549fdb8b54f9b
Roberta Araujo <160649185+Roberllie@users.noreply.github.com>
2025-11-17 17:16:22 -0300

    Create aluno.sql

---
+++ aluno.sql
@@ -0,0 +1,5 @@
+INSERT INTO aluno (nome, matricula, curso, ano_ingresso)
+VALUES
+('Roberta Souza', '2025001', 'Engenharia de Computação', 2025),
+('Lucas Andrade', '2025002', 'Sistemas de Informação', 2025),
+('Maria Silva', '2025003', 'Engenharia Elétrica', 2024);
